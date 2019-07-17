8f4dc65213140f1f05dde5ee95ec4d702c8dc1d7
zhangmn <zhangmn@asiainfo.com>
2019-07-17 11:19:12 +0800

    添加预发布

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,5 +15,22 @@ module.exports = {
       NODE_ENV: 'development',
       PORT: 3019,
     },
+  },
+  {
+    name: 'config_stage',
+    script: '.',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: '',
+    instances: '2',
+    autorestart: true,
+    kill_timeout: 10 * 1000, // 从发出停止 SIGINT 信号到强制停止等待的时间，给一分钟在途请求处理时间
+    wait_ready: true,
+    watch: false,
+    max_memory_restart: '100M',
+    merge_logs: true,
+    env: {
+      NODE_ENV: 'development',
+      PORT: 2019,
+    },
   }],
 };
